9c89c09614f058f945f857c278bc0582620169a5
ranjitabhajantri2007-web <ranjitabhajantri2007@gmail.com>
2026-03-27 01:36:30 -0700

    Add files via upload

---
+++ param.py
@@ -0,0 +1,12 @@
+tup1=["ramesh","mbbs",2000,202212034]
+print(tup1)
+tup2=["satish","engneering",2001,10130456]
+print(tup2)
+tup3=tup1+tup2
+print(tup3)
+del tup1
+print("after deleteing tup1:")
+print(tup1)
+
+
+
